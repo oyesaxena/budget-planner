02f228aff9399611b88c23ca737ab17abcd4aeee
Abhishek Saxena2 <abhisheksaxena2@M-C56R6F7MCW.local>
2024-04-15 23:45:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'flexi';
+  numberOfChars=0;
+  @ViewChild ('charsModel',{static:true}) viewVar :any;
+  allVowels:any=[]
+
+
+  changeFun(e:any){
+    this.numberOfChars=e.target.value.length;
+    this.allVowels=[];
+    for(let i=0;i<this.title.length;i++){
+      if(['a','e','i','o','u'].includes(this.title[i].toLowerCase())){
+        this.allVowels.push(this.title[i])
+      }
+    }
+    console.log('allVowels',this.allVowels);
+
+  }
+  trackByFn(index:any,item:any){
+    return item;
+  }
+
 }
